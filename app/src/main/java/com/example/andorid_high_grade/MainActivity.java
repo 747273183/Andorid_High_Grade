c9b7d85c86b7d393ec3e6b16219a1a29af14249d
森林之王 <747273183@qq.com>
2019-08-18 16:22:08 +0800

    1.启动服务和停止服务

--- app/src/main/java/com/example/andorid_high_grade/MainActivity.java
+++ app/src/main/java/com/example/andorid_high_grade/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.andorid_high_grade;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_start)
+    Button btnStart;
+    @BindView(R.id.btn_stop)
+    Button btnStop;
+    @BindView(R.id.btn_bind)
+    Button btnBind;
+    @BindView(R.id.btn_unbind)
+    Button btnUnbind;
+    private Intent service;
+
+    private static final String TAG = "MyService-1";
+    private ServiceConnection conn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        service = new Intent(MainActivity.this,MyService.class);
+
+
+    }
+
+    @OnClick({R.id.btn_start, R.id.btn_stop, R.id.btn_bind, R.id.btn_unbind})
+    public void
+    onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_start:
+                startService(service);
+                break;
+            case R.id.btn_stop:
+                stopService(service);
+                break;
+            case R.id.btn_bind:
+
+                break;
+            case R.id.btn_unbind:
+
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/andorid_high_grade/MyService.java
@@ -0,0 +1,48 @@
+package com.example.andorid_high_grade;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    private static final String TAG = "MyService-1";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+        Log.d(TAG, "onRebind: ");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind: ");
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG, "onUnbind: ");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+}
