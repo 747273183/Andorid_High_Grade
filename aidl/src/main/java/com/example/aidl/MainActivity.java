59484ad92b8342538262699b91ddfedc0743e59e
森林之王 <747273183@qq.com>
2019-08-20 15:46:51 +0800

    5.远程启动服务,远程停止服务,远程绑定服务,远程解绑服务

--- aidl/src/main/java/com/example/aidl/MainActivity.java
+++ aidl/src/main/java/com/example/aidl/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.aidl;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -23,27 +26,47 @@ public class MainActivity extends AppCompatActivity {
     Button btnUnbind;
     @BindView(R.id.progressBar)
     ProgressBar progressBar;
+    private Intent intent;
+    private ServiceConnection conn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+
+        intent = new Intent();
+        intent.setAction("com.action.servicedemo");
+        intent.setPackage("com.example.service");
+        conn=new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+
+
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+
+            }
+        };
     }
 
     @OnClick({R.id.btn_start, R.id.btn_stop, R.id.btn_bind, R.id.btn_unbind})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_start:
-                Intent intent=new Intent();
-                intent.setAction("com.action.servicedemo");
-                intent.setPackage("")
+                startService(intent);
                 break;
             case R.id.btn_stop:
+                stopService(intent);
                 break;
             case R.id.btn_bind:
+                bindService(intent,conn,BIND_AUTO_CREATE);
                 break;
             case R.id.btn_unbind:
+                unbindService(conn);
                 break;
         }
     }
