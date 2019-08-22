54c72d10c5ce01c1e4a44d3497f0a533bf3345d5
森林之王 <747273183@qq.com>
2019-08-22 18:08:03 +0800

    12.使用UriMatcher匹配操作路径

--- contentprovider/src/main/java/com/example/contentprovider/MyContentProvider.java
+++ contentprovider/src/main/java/com/example/contentprovider/MyContentProvider.java
@@ -1,24 +1,40 @@
 package com.example.contentprovider;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
+import android.util.Log;
 
 public class MyContentProvider extends ContentProvider {
 
     public static final String TABLE = "info_tb";
+    public static final int MATCHER_CODE = 1000;
     private SQLiteDatabase db;
+    private UriMatcher matcher;
+
+    private static final String TAG = "MyContentProvider";
 
     public MyContentProvider() {
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        int match = matcher.match(uri);
+        if (match==MATCHER_CODE)
+        {
+            return db.delete(TABLE,selection, selectionArgs);
+        }
+        else
+        {
+            Log.d(TAG, "delete: path不匹配");
+        }
+        return 0;
+
     }
 
     @Override
@@ -30,8 +46,21 @@ public class MyContentProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
+        int match = matcher.match(uri);
+        long id=0;
+        if (match==MATCHER_CODE)
+        {
+          id = db.insert(TABLE, null, values);
+
+        }
+        else
+        {
+            Log.d(TAG, "insert: path不匹配");
+
+        }
+
+        return ContentUris.withAppendedId(uri,id);//返回新添加的数据id
+
     }
 
     @Override
@@ -55,19 +84,38 @@ public class MyContentProvider extends ContentProvider {
             }
         };
         db = helper.getReadableDatabase();
+
+        //路径匹配
+        matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        matcher.addURI("com.wanglin.myprovider","/insert",MATCHER_CODE);
+        matcher.addURI("com.wanglin.myprovider","/delete",MATCHER_CODE);
+        matcher.addURI("com.wanglin.myprovider","/update",MATCHER_CODE);
+        matcher.addURI("com.wanglin.myprovider","/query",MATCHER_CODE);
         return true;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
-       return  db.rawQuery("select * from "+TABLE+" order by  _id desc",null);
+
+            return  db.rawQuery("select * from "+TABLE+" order by  _id desc",null);
+
+
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        int match = matcher.match(uri);
+        if (match==MATCHER_CODE)
+        {
+            return db.update(TABLE,values,selection,selectionArgs);
+        }
+        else
+        {
+            Log.d(TAG, "update: path不匹配");
+            return 0;
+        }
+
     }
 }
--- contentresolver/src/main/java/com/example/contentresolver/MainActivity.java
+++ contentresolver/src/main/java/com/example/contentresolver/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.contentresolver;
 
-import android.annotation.SuppressLint;
 import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
@@ -10,6 +11,7 @@ import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -17,6 +19,9 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.contentresolver.bean.Person;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -49,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
     private SimpleCursorAdapter adapter;
 
     private static final String TAG = "MainActivity";
+    private Person person;
+    private long id;
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -62,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         resolver=getContentResolver();
 
         //给listview设置适配器,让listview显示数据
-        cursor = resolver.query(Uri.parse(URI), null, null, null);
+        cursor = resolver.query(Uri.parse(URI+"/query"), null, null, null);
         Log.d(TAG, "onCreate: "+cursor.getCount());
         String[] from={"_id","name","gender","age"};
         int[] to={R.id.tv_id,R.id.tv_name,R.id.tv_gender,R.id.tv_age};
@@ -70,19 +77,65 @@ public class MainActivity extends AppCompatActivity {
                 cursor,from,to,SimpleCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
         lv.setAdapter(adapter);
 
+        //list列表项被单击事件
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+              Cursor cursor= (Cursor) parent.getItemAtPosition(position);
+                int _id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String gender = cursor.getString(2);
+                int age = cursor.getInt(3);
+
+                tvId.setText(_id+"");
+                etName.setText(name);
+                if ("男".equals(gender))
+                {
+                    rbMan.setChecked(true);
+                }
+                else
+                {
+                    rbWoman.setChecked(true);
+                }
+
+                etAge.setText(age+"");
+
+                MainActivity.this.id=_id;
+            }
+        });
+
 
     }
 
     @OnClick({R.id.btn_add, R.id.btn_update, R.id.btn_del})
     public void onClick(View view) {
+        String name = etName.getText().toString();
+        RadioButton radioButton=  rgSex.findViewById( rgSex.getCheckedRadioButtonId());
+        String gender = radioButton.getText().toString();
+        Integer age =new Integer(etAge.getText().toString()) ;
+        ContentValues values=new ContentValues();
+        values.put("name",name);
+        values.put("gender",gender);
+        values.put("age",age);
+
+
         switch (view.getId()) {
             case R.id.btn_add:
-
+                Uri uri = resolver.insert(Uri.parse(URI+"/insert"), values);
+                long id = ContentUris.parseId(uri);
+                Toast.makeText(this, "添加成功,新数据id="+id, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_update:
+                values.put("_id", this.id +"");
+                int update = resolver.update(Uri.parse(URI+"/insert"), values, "_id=?", new String[]{this.id + ""});
+                Toast.makeText(this, "修改成功,受影响的记录行数="+update, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_del:
+                int delete = resolver.delete(Uri.parse(URI+"/insert"), "_id=?", new String[]{this.id + ""});
+                Toast.makeText(this, "删除成功,受影响的记录行数="+delete, Toast.LENGTH_SHORT).show();
                 break;
         }
+        adapter.notifyDataSetChanged();
+
     }
 }
